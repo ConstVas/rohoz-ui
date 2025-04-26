89be4e3d07d7c2cdcfc616eb21eaef01feaee39b
Konstantin Vasiliev <konstantin.vasiliev@zyfra.com>
2025-04-26 21:56:25 +0300

    squash commits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,23 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterOutlet } from '@angular/router';
 import { RhzInputComponent } from './rhz-input/rhz-input.component';
-import { CommonModule } from '@angular/common';
-import { catchError, filter, from, interval, switchMap, take, tap, throwError } from 'rxjs';
-import { HttpClient, HttpClientModule, HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule,
-    RouterOutlet, FormsModule, RhzInputComponent,
-    HttpClientModule],
+  imports: [
+    FormsModule, RhzInputComponent,
+    ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less'
 })
 export class AppComponent {
   title = 'rohoz-ui';
 
-  value: any;
-
   defaultValue = 1232333;
-  log: any
 
   inputChange($event: any) {
     console.log($event);
-    this.log = $event;
   }
 
-  constructor(private http: HttpClient) {
-
-  }
 }
